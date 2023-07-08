3a29628a49fd63e456d626bd5e68d2703ef88cbd
Lewis Gaul <lewis.gaul@gmail.com>
2023-07-09 00:08:08 +0100

    Add setup script for deploying symlinks to HOME

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+for from_filepath in $(find "$THIS_DIR/homedir" -type f); do
+    file=$(realpath --relative-to="$THIS_DIR/homedir" "$from_filepath")
+    to_filepath=$HOME/$file
+    if [[ $from_filepath == "$HOME/"* ]]; then
+        from_symlink=$(realpath --relative-to="$HOME" "$from_filepath")
+    else
+        from_symlink=$from_filepath
+    fi
+    if [[ -e $to_filepath ]]; then
+        if diff "$from_filepath" "$to_filepath"; then
+            echo "File $file already deployed"
+            continue
+        fi
+        echo "Skipping $file as it already exists, please remove and rerun" >&2
+    else
+        echo "Symlinking $file"
+        ln -s "$from_symlink" "$to_filepath"
+    fi
+done
