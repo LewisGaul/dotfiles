b769c3a3a2ef7f006e0755bf93827ebc4007bd2e
Lewis Gaul <lewis.gaul@gmail.com>
2024-01-16 18:34:21 +0000

    Add fixclock alias

--- bashrc/wsl.sh
+++ bashrc/wsl.sh
@@ -3,3 +3,4 @@
 alias powershell='/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe'
 alias fixdns='powershell -Command "Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses" | tac | sed "s/^/nameserver /" | dos2unix | sudo tee /etc/resolv.conf'
 alias ssh_restart='sudo service ssh --full-restart'
+alias fixclock='sudo ntpdate time.windows.com'
